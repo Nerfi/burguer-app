f1723900073ae862d7ad510a3e744e6697869373
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-03-30 12:16:18 +0200

    array mapped the array

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -5,11 +5,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+  // props ingredients here is an object and we are converting that object into an array of keys
+  const transformIngredients = Object.keys(props.ingredients)
+      .map(igKey => {
+        //props.ingredients[igKey] is the number of the current ingredient (singular).
+        //So 5 slices of cheese is 5.
+          return [...Array(props.ingredients[igKey])].map((_,index) => {
+           return <BurgerIngredient key={igKey + index} type={igKey}/>
+          });
+      });
+
   return(
     <div className="Burger">
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {transformIngredients}
       <BurgerIngredient type="bread-botton"/>
     </div>
 
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -3,10 +3,20 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burguer/Burguer';
 
 class BurguerBuilder extends Component {
+  //the new way of defined state
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon:0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
   render(){
     return(
       <Aux>
-      <Burger/>
+      <Burger ingredients={this.state.ingredients}/>
 
         <div>Build controll</div>
       </Aux>
